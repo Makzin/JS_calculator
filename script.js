0a05b7943ff37ebafcf1520c1ff4bb3b5b3e9267
Max Van Lyl <maximvanlyl@gmail.com>
2017-01-03 18:29:35 -0500

    fixed some calc issues

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var result = 0; // whatever will be displayed in the result screen
 var resultCheck = 0;
 var reset = ''; // the clear button
 var opEntry = ''; // the operator entry
+var opCounter = 0;
 
 // operators object to reference later
 var operators = {
@@ -24,32 +25,49 @@ combinedDigits = '';
 buttonEntry = $(this).text();
 intermediateEntry += buttonEntry;
 combinedDigits = parseInt(intermediateEntry);
+console.log("CombinedDigits while pressing a number: " + combinedDigits);
 $('#result').html('<p>' + intermediateEntry + '</p>')
 })
 
 $('.operator').on('click', function() {
-opEntry = $(this).text();
-resultCheck = operators[opEntry](previousEntry, combinedDigits );
-  console.log('previousEntry before if statement : ' + previousEntry);
-  if (resultCheck == 0) {
-    previousEntry = combinedDigits;
-  }
-  else if (resultCheck < 0 && combinedDigits > 0) {
-    previousEntry = -Math.abs(result)
-           }
-  else {
-    previousEntry = resultCheck;
-  }
-intermediateEntry = '';
-console.log('previousEntry after if statement : ' + previousEntry);
+  opEntry = $(this).text();
 
+console.log("PreviousEntry before if statement: " + previousEntry)
+console.log("CombinedDigits before if statement: " + combinedDigits)
+ // previousEntry will always be 0 when the operator button is pressed for the first time, so we need to assign them whatever digits were entered in the first round.
+if (opCounter == 0 && opEntry != 'x' && opEntry != '/') {
+ result = operators[opEntry](previousEntry, combinedDigits)
+ previousEntry = result;
+ console.log("it's using the first if statement!")
+ combinedDigits = 0;
+
+}
+else if (opCounter == 0 && (opEntry == 'x' || opEntry == '/')) {
+  result = operators[opEntry](combinedDigits, 1)
+  previousEntry = result;
+   console.log("it's using the second if statement!")
+  combinedDigits = 0;
+}
+
+else {
+  result = operators[opEntry](previousEntry, combinedDigits)
+  previousEntry = result;
+   console.log("it's using the third if statement!")
+}
+
+intermediateEntry = '';
+opCounter++;
+console.log('previousEntry AFTER if Statement : ' + previousEntry);
+  console.log("CombinedDigits AFTER if statement: " + combinedDigits)
 })
+
 
 $('.equal').on('click', function() {
   result = operators[opEntry](previousEntry, combinedDigits)
   $('#result').html('<p>' + result + '</p>')
   console.log('final result: ' + result);
 })
+
 
 $('.clear').on('click', function() {
   $('#result').html('<p>');
@@ -58,6 +76,7 @@ $('.clear').on('click', function() {
   combinedDigits = '';
   buttonEntry = '';
   intermediateEntry = '';
+  opCounter = 0;
 })
 });
 
